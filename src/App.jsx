652467404a44fc744af69ccf4b7827cd4aa13460
tiagolimar <tiagolima.r2@gmail.com>
2024-02-02 18:33:04 -0300

    add files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from './components/Navbar';
+import { Carrousel } from './components/Carrousel';
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+            <Carrousel />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Carrousel.jsx
@@ -0,0 +1,6 @@
+
+export const Carrousel = () => {
+  return (
+    <div>Carrousel</div>
+  )
+}
